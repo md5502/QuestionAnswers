4db23bd0f4fb0faa7ba540abba0656156edd785d
mohammad <m.baniasadi.d@gmail.com>
2023-01-10 22:55:16 +0330

    add components to the app file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Header_component :correctCount="correctCount" :totalCount="totalCount" />
+	<Content_component v-if="questionlist.length" :question="questionlist[index]" :next="next"
+		:checkAnswer="checkAnswer" :msg="msg" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header_component from './components/Header_component.vue';
+import Content_component from './components/Content_component.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: 'App',
+	components: {
+		Header_component,
+		Content_component
+	},
+	data() {
+		return {
+			questionlist: [],
+			index: 0,
+			correctCount: 0,
+			totalCount: 0,
+			msg : ''
+		}
+	},
+	mounted: function () {
+		fetch('https://opentdb.com/api.php?amount=20').then((re) => {
+			return re.json()
+		}).then((result) => {
+			this.questionlist = result.results;
+		})
+	},
+	methods: {
+		next() {
+			if (this.index < this.questionlist.length - 1) {
+				this.index++;
+			}
+		},
+		checkAnswer(is_correct) {
+			if (is_correct) {
+				this.correctCount++;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+			}
+			if(this.totalCount < this.questionlist.length){
+				this.totalCount++;
+			}if(this.totalCount === this.questionlist.length){
+				this.msg = `You finish the exam and you answer ${this.correctCount} out of ${this.totalCount} Correctly`
+			}
+			
+		}
+	}
 }
-</style>
+
+
+</script>
