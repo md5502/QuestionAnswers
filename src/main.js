512822f32557a6b9e97a53edf265c16c7e3c1d30
mohammad <m.baniasadi.d@gmail.com>
2023-01-10 22:46:39 +0330

    importing popper

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+
+
 
 createApp(App).mount('#app')
